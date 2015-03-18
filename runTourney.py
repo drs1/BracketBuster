6f8f671fe568f7efe00be7083636fcf6932de919
Danny Smith <drs1@williams.edu>
2015-03-18 18:48:30 -0400

    directory cleanup

--- runTourney.py
+++ runTourney.py
@@ -6,7 +6,7 @@ import time
 stats = ['second_chance_pts', 'points_off_turnovers','paint_pts', 'fast_break_pts', 'points', 'blocked_att', 'turnovers' ,'free_throws_att', 'field_goals_att', 'off_rebounds', 'two_points_att', 'assists', 'blocks', 'personal_fouls', 'two_points_made', 'flagrant_fouls', 'rebounds', 'three_points_made', 'minutes', 'def_rebounds', 'free_throws_made', 'field_goals_made', 'steals', 'three_points_att']
 
 def pickTourneyOn(stat):
-    marchMadness = ET.parse('2015MarchMadness.xml')
+    marchMadness = ET.parse('xml/2015MarchMadness.xml')
     #we are in round 2
     round2 = marchMadness.getroot()[0][1]
     for r in round2:
